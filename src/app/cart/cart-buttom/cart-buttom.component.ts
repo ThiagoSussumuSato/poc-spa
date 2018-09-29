771680fd73910ec451c7046c8db26b758a28f8f9
Thiago Sussumu Sato <sato.sussumu@gmail.com>
2018-09-29 19:56:51 -0300

    Ajuste Cart

--- src/app/cart/cart-buttom/cart-buttom.component.ts
+++ src/app/cart/cart-buttom/cart-buttom.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -7,17 +7,17 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart-buttom.component.css']
 })
 export class CartButtomComponent implements OnInit {
-
+  
   qtdProduto: number;
   valorTotal: number;
-
+  
   constructor(private cartService: CartService) { 
     this.cartService.update.subscribe(valor => {
       this.qtdProduto = this.cartService.obterQuantidadeProduto();
       this.valorTotal = this.cartService.obterValorTotal();
     });
   }
-
+  
   ngOnInit() {
     this.qtdProduto = this.cartService.obterQuantidadeProduto();
     this.valorTotal = this.cartService.obterValorTotal();
